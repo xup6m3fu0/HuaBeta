d5666b724de37b780de24185b1403403e91ac10d
xup6m3fu0 <xup6m3fu0@gmail.com>
2017-02-23 15:07:54 +0800

    2017_2

---
+++ app/controllers/mains_controller.rb
@@ -0,0 +1,7 @@
+class MainsController < ApplicationController
+
+	def index 
+
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'mains#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -47,6 +47,7 @@ Rails.application.routes.draw do
   #   resources :posts, concerns: :toggleable
   #   resources :photos, concerns: :toggleable
     resources :users,only:[:new,:create]
+    resources :mains,only:[:index]
 
   # Example resource route within a namespace:
   #   namespace :admin do
